4f5d3b3e6ee57a78953d8b6a891d3ecbec1be50d
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-04-02 14:00:06 +0530

    feat: link

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,13 +1,22 @@
 import { Button } from "./button";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div>
       <div className="space-x-4 justify-center flex p-2">
-        <Button>Home</Button>
-        <Button>Projects</Button>
-        <Button>Experience</Button>
-        <Button>Blogs</Button>
+        <Button asChild>
+          <Link href="/Home">Home</Link>
+        </Button>
+        <Button asChild>
+          <Link href="/projects">Projects</Link>
+        </Button>
+        <Button asChild>
+          <Link href="/Experience">Experience</Link>
+        </Button>
+        <Button asChild>
+          <Link href="/Blogs">Blogs</Link>
+        </Button>
       </div>
       <div className="border-b-2"></div>
     </div>
