2d0b0c5bb0d69fec3e2b0daa93475b227cad78e6
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-04-03 16:37:20 +0530

    fix: home

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,25 @@ export default function Home() {
     <main className="">
       <div className="grid grid-cols-5">
         <div className="col-span-3">
-          <div className="text-5xl font-bold pt-4">
+          <div className="text-6xl font-bold pt-4">
             👋🏻 Hey, I&apos;m <text className="text-indigo-600">Pradhumn!</text>
           </div>
           <br />
-          <div>Building seamless web solutions</div>
-          (@iPradhumngautam) a Full Stack Developer from India. Passionate about
-          Software Development, Cloud Engineering, Backend Systems & Open-Source
-          Softwares. Currently an undergraduate, pursuing Computer Science
-          Engineering majors from Maharaja Agrasen Institue of Technology, New
-          Delhi, India.
+          <div className="text-3xl font-bold">
+            Building seamless web solutions
+          </div>
+          <br />
+          <div className="text-lg">
+            I&apos;m Pradhumn Gautam a Full Stack Developer from India.
+            Passionate about Software Development, Cloud Engineering, Backend
+            Systems & Open-Source Softwares.
+          </div>
+          <br />
+          <div className="text-lg">
+            I&apos;m currently an undergraduate, pursuing Computer Science
+            majors with Specialization in Artificial Intelligence and Machine
+            Learning from New Delhi, India.
+          </div>
         </div>
         <div></div>
       </div>
