d693f9c0aef96be4b5643229f8a4bc54564fdecd
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-04-03 16:11:30 +0530

    feat: home

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 export default function Home() {
   return (
     <main className="">
-      <div>
-        👋🏻 Hey I&apos;m Pradhumn Gautam (@iPradhumngautam) a Full Stack
-        Developer from India. Passionate about Software Development, Cloud
-        Engineering, Backend Systems & Open-Source Softwares. Currently an
-        undergraduate, pursuing Computer Science Engineering majors from
-        Maharaja Agrasen Institue of Technology, New Delhi, India.
+      <div className="grid grid-cols-5">
+        <div className="col-span-3">
+          <div>👋🏻 Hey I&apos;m Pradhumn Gautam</div> (@iPradhumngautam) a Full Stack
+          Developer from India. Passionate about Software Development, Cloud
+          Engineering, Backend Systems & Open-Source Softwares. Currently an
+          undergraduate, pursuing Computer Science Engineering majors from
+          Maharaja Agrasen Institue of Technology, New Delhi, India.
+        </div>
+        <div></div>
       </div>
     </main>
   );
