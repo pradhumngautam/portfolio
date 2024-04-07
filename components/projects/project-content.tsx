02b972378ada265c81e0e656e069ac6d24d5e2a5
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-04-07 16:00:58 +0530

    feat: projects added

--- components/projects/project-content.tsx
+++ components/projects/project-content.tsx
@@ -15,15 +15,15 @@ export const projectsContent: projectData[] = [
     description:
       "Medium is a blogging website which allows user to do CRUD operations on their blogs.Hono.js library of wrangler is used here for writing backend server which is hosted on cloudflare workers, connection-pooling is used for maintaining postgress requests.",
     tags: [
-        "Typescript",
-        "React.js",
-        "Hono.js",
-        "Prisma",
-        "PostgreSQL",
-        "Connection - pooling",
-        "Custom hooks",
-        "TailwindCSS",
-        "ShadcnUI"
+      "Typescript",
+      "React.js",
+      "Hono.js",
+      "Prisma",
+      "PostgreSQL",
+      "Connection - pooling",
+      "Custom hooks",
+      "TailwindCSS",
+      "ShadcnUI",
     ],
     link: {
       github: "https://github.com/pradhumngautam/medium",
@@ -32,23 +32,22 @@ export const projectsContent: projectData[] = [
     },
   },
   {
-    title: "Typewriter Playground",
+    title: "Swift Spend",
     description:
-      "A real-time typwriter speed test app for competing with your friends online and compare the typing speed.",
+      "SWIFT SPEND is a banking app which enables user to transfer money to other users. backend involves RESTful APIs and custom token-based authentication for secure access control including the password hashing through bCrypt, sessions are used for making transactions for controlling multiple transfer requests on same account or in the case of server/database crash.",
     tags: [
-      "Typescript",
-      "Node.js",
-      "Next.js",
-      "Prisma",
-      "Socket.IO",
-      "TailwindCSS",
-      "Turborepo(Monorepo)",
-      "Shadcn UI",
+        "Javascript",
+        "Node.js",
+        "Express.js",
+        "Mongoose",
+        "MongoDB",
+        "JWT",
+        "bCrypt"
     ],
     link: {
-      github: "https://github.com/tanayvaswani/typewriter-playground",
+      github: "https://github.com/pradhumngautam/paytm",
       youtube: "",
-      live: "",
+      live: "https://paytm-gules.vercel.app/",
     },
   },
 ];
