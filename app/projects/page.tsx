7d808e22c2e8652a331d760529a0d07ac10c9fba
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-04-07 16:25:39 +0530

    feat: projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,26 @@
 import ProjectCard from "@/components/projects/project-card";
+import { projectsContent } from "@/components/projects/project-content";
 
 const Projects = () => {
   return (
-    <div>
-      
-    </div>
+ 
+      <div className="my-8 md:my-20 w-full md:max-w-screen-2xl md:mx-auto md:px-16">
+        <ul className="flex flex-col items-center justify-center w-full">
+          {projectsContent.map((card, idx) => {
+            return (
+              <ProjectCard
+                title={card.title}
+                header={card.header}
+                description={card.description}
+                tags={card.tags}
+                key={idx}
+                link={card.link}
+              />
+            );
+          })}
+        </ul>
+      </div>
+  
   );
 };
 
