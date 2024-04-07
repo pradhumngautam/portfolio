f169ce61ba73e6dad729b8ee9d2412ec970dd554
Pradhumn Gautam <pradhumngautam0506@gmail.com>
2024-04-07 17:46:45 +0530

    feat: button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -25,20 +25,19 @@ const Navbar = () => {
     <div className="flex  items-center h-16 border-b bg-zinc-950 fixed w-full shadow-md justify-between p-5">
       <div className="flex items-center justify-center gap-3">
         {navItems.map((item, index) => (
-          <Button className="rounded-full" asChild key={index}>
+          <Button className="rounded-full text-md font-semibold bg-blue-500 shadow-md hover:bg-blue-600 " asChild key={index}>
             <Link href={item.src}>{item.name}</Link>
           </Button>
         ))}
       </div>
       <Button
-            className="bg-gradient-to-r from-blue-600 to-blue-900 text-neutral-100 font-semibold text-md"
-            size={"sm"}
-            asChild
-          >
-            <Link href={""}>Contact Me</Link>
-          </Button>
+        className="bg-gradient-to-r from-blue-600 to-blue-900 text-neutral-100 font-semibold text-md"
+        size={"sm"}
+        asChild
+      >
+        <Link href={""}>Contact Me</Link>
+      </Button>
     </div>
-    
   );
 };
 
